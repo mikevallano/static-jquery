2f2639cea77b9dbd5fac1d106f48e27da8c17b03
mike vallano <mikevallano@gmail.com>
2017-03-26 08:50:59 -0500

    Add syntax for event listeners added to the DOM after .ready

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ $(document).ready(function(){
     $('#especially').removeClass('hidden')
   })
 });
+
+// If you are adding an event listener to items added to the DOM w/ JS,
+// use this syntax. document.ready only applies to items in the DOM when the
+// page initially loads (is ready).
+$(document).on('click', '.element', function() {
+  // do stuff
+});
